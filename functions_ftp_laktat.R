3fd3b778be3bedddca7432250eb49c05a1f0a111
Rigler Andreas <riglerandreas@gmx.at>
2022-11-26 00:00:04 +0100

    adde plot function laktat curves

--- functions_ftp_laktat.R
+++ functions_ftp_laktat.R
@@ -144,3 +144,89 @@ select_ftp_mmol <- function(df){
     
   return(result)
 }
+
+#========================================================================================
+watt_laktat <- function(d, start_b = 0.1){
+  #predict the laktat values for different watt values, based on the laktat measurements
+  
+  #start_b was 1, but 0.1 works better
+  
+  result <-tibble()
+  #fehlmessungen filter: (laktat muss steigen)
+  d <- d %>% arrange(watt) %>% mutate(ungueltig = laktat-lag(laktat, 
+                                                             default =0)) %>% 
+    filter(ungueltig >0) %>% select(-ungueltig)
+  
+  model_laktat <- nls(watt~a*log(b*laktat),data = d,start = list(a=10,b=start_b))
+  
+  
+  
+  laktat_prediction <- d %>% data_grid(laktat = seq_range(laktat,200)) %>%
+    add_predictions(model_laktat,"watt") 
+  date <- d %>% summarise(mean(date)) %>% pull()
+  watt <- laktat_prediction %>% filter(laktat <= 2) %>% summarise(watt = max(watt)) %>% pull()
+  
+  result <- tibble("date" = date,
+                   "watt_2mmol" = watt)
+  return(laktat_prediction)
+  
+  
+}
+
+#-----------------------------------------------------------------------------------
+
+
+plot_laktat_curves <- function(laktat_measurements, laktat_results){
+  
+  # get all dates from the data:
+  dates_all <- laktat_measurements %>% bind_rows(laktat_results)%>% 
+    mutate(date=as.Date(date)) %>% distinct(date) 
+  
+  
+  
+  for (i in seq_along(dates_all$date)) {
+    
+    data_test <- data_laktat_measurements %>% filter(as.Date(date) == as.Date(dates_all$date[i]))  
+    
+    laktat_results_sel <- laktat_results %>% filter(as.character(date) == as.character(dates_all$date[i]))  
+    
+    
+    
+    
+    plot_1 <- data_test %>% ggplot(aes(y=laktat)) + 
+      geom_point(aes(x= watt)) + 
+      geom_line(aes(x= watt)) + 
+      geom_hline(yintercept = c(2,5))+
+      geom_vline(xintercept = c(190,200,210, 240,250,260,270), linetype =1, color = "red")
+    
+    # plot the 
+    if (dim(laktat_results_sel)[1]>0) {
+      plot_1 <- plot_1 + 
+        geom_point(data = laktat_results_sel,aes(x = w_5mmol, y=5, color = "calc"), size =5)+
+        geom_point(data = laktat_results_sel,aes(x = w_2mmol, y=2, color = "calc"), size =5)+ 
+        labs(subtitle = str_c("2 mmol: ",round(laktat_results_sel$w_2mmol, digits = 2),
+                              "    5 mmol: ",round(laktat_results_sel$w_5mmol, digits = 2) ))
+    }
+    
+    # PRedict the watt laktat values and plot them
+    data_predict <- c()
+    try(data_predict <- watt_laktat(data_test, start_b = 0.01), silent = TRUE)
+    
+    if (is_empty(c(data_predict)) == FALSE) {
+      plot_1 <- plot_1 + geom_point(data = data_predict,aes(x= watt))
+    }    
+    
+    
+    #Add title to the plot  
+    plot_1 <- plot_1 + 
+      labs(title = str_c("date: ",dates_all$datum[i] ))+
+      theme_light() 
+    
+    print(plot_1)
+  }
+  
+  
+  
+  #return(data_predict)
+  
+}
